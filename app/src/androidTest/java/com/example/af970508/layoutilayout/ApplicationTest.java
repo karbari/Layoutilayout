ff0271d350574cf2515b55c91ab4d03fc24af5f7
Farhatnia <ali.farhatnia@skola.gavle.se>
2016-09-22 10:44:37 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/example/af970508/layoutilayout/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.af970508.layoutilayout;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
